0ee92cdccbd198609ab83315d4506e15719ba2d0
KonstantinZakrzhevskyi <konstantinz2505@gmail.com>
2021-11-09 05:15:31 +0200

    Добавляет все компоненты и стилизацию

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,28 +1,28 @@
 import { Component } from 'react';
-import shortid from 'shortid';
+import PropTypes from 'prop-types';
+import s from './ContactForm.module.css';
 
 class ContactForm extends Component {
-
+  static propTypes = {
+    onSubmit: PropTypes.func.isRequired,
+  };
+  
   state = {
     name: '',
-    number: ''
+    number: '',
   };
-
-  nameInputId = shortid.generate();
-  numberInputId = shortid.generate();
 
   handleInputChange = e => {
     const { name, value } = e.currentTarget;
     this.setState({ [name]: value });
-  }
+  };
 
   handleSubmit = e => {
     e.preventDefault();
 
     this.props.onSubmit(this.state);
-
     this.reset();
-  }
+  };
 
   reset = () => {
     this.setState({name: '', number: ''})
@@ -32,23 +32,22 @@ class ContactForm extends Component {
     const { name, number } = this.state;
 
     return (
-      <form onSubmit={this.handleSubmit}>
-        <label htmlFor={this.nameInputId}>
+      <form className={s.form} onSubmit={this.handleSubmit}>
+        <label className={s.form__label}>
           Name
-      <input
-        type="text"
-        name="name"
-        value={name}
-        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-        title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
-        required
-        onChange={this.handleInputChange}
-        id={this.nameInputId}
-        />
+          <input className={s.form__input}
+            type="text"
+            name="name"
+            value={name}
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+            required
+            onChange={this.handleInputChange}
+          />
         </label>.
-        <label htmlFor={this.numberInputId}>
+        <label className={s.form__label}>
           Number
-          <input
+          <input className={s.form__input}
             type="tel"
             name="number"
             value={number}
@@ -56,13 +55,12 @@ class ContactForm extends Component {
             title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
             required
             onChange={this.handleInputChange}
-            id={this.numberInputId}
           />
         </label>
-        <button type="submit" disabled={!name}>Add contact</button>
-        </form>
-    )
-  }
+        <button className={s.form__btn} type="submit" disabled={!name}>Add contact</button>
+      </form>
+    );
+  };
 };
 
 export default ContactForm;
--- src/components/Section/Section.js
+++ src/components/Section/Section.js
@@ -2,15 +2,17 @@ import PropTypes from "prop-types";
 import s from './Section.module.css'
 
 const Section = ({title, children}) => {
- return (<section className={s.section}>
-    <h2 className={s.section__title}>{title}</h2>
-   {children}
- </section> ) 
+  return (
+    <section className={s.section}>
+      {title && <h2 className={s.section__title}>{title}</h2>}
+      {children}
+    </section>
+ )
 }
 
 Section.propTypes = {
-  title: PropTypes.string.isRequired,
-  children: PropTypes.node.isRequired
+  title: PropTypes.string,
+  children: PropTypes.node,
 }
 
 export default Section;
